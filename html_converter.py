4a7871fad035c08aafd6d94cf008823d14d746f4
rogcomfox <joeg.11021996@gmail.com>
2026-03-06 16:30:53 +0700

    fix: logo design

--- html_converter.py
+++ html_converter.py
@@ -1,2 +1,6 @@
-import html2image
+from html2image import Html2Image
 
+hti = Html2Image()
+file_path = '/Users/joeg/Project/Logo/logo-design-html/design/zero_nine_pandan.html'
+out_name = 'pandan_logo.png'
+hti.screenshot(html_file=file_path, save_as=out_name)
